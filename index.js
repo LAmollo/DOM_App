cf9ca7fac56d24776f15524801f1d0aff9e6445e
LAmollo <lawinyamuga@gmail.com>
2024-05-21 22:26:12 -0700

    js file/funct

--- index.js
+++ index.js
@@ -15,3 +15,6 @@ weatherForm.addEventListener('submit', function(event) {
     }
 });
 
+function fetchWeather(city) {
+    const apiKey = '0ce3ad64e1e026ca8eae0a7e795ae30e';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
